6a325ee6a3d665eac5d8a87ab0c9f4850a2e24d2
GTorralvo <gamestorralvo002@gmail.com>
2026-03-02 10:21:12 -0300

    estamos quase la

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert("Eu funcionei")
+const numero1 = document.getElementById("numero1")
+const numero2 = document.getElementById("numero2")
+const button = document.getElementById("button")
+const resultado = document.getElementById("resultado")
+
+function calculaMedia(){
+    const nota1 = Number (numero1.value)
+    const nota2 = Number (numero2.value)
+const media = (nota1 + nota2)/2   
+
+    console.log (nota1 + nota2)
+
+    resultado.innerHTML = `A media e ${media}`
+}
+``
+button.addEventListener("click", calculaMedia)
